3df9de82b7e05c3ec97f8d4ee7a63a750c91a754
shlomota <shlomotannor@mail.tau.ac.il>
2021-09-02 23:15:30 +0300

    fixed train_quora

--- train_quora.py
+++ train_quora.py
@@ -40,8 +40,31 @@ if is_university():
 
 
 
+# def tokenize_function(examples, tokenizer):
+#     dic = tokenizer(examples["text1"], padding="max_length", truncation=True)
+#     for k in dic.keys():
+#         dic[k + "1"] = dic.pop(k)
+#
+#     dic2 = tokenizer(examples["text2"], padding="max_length", truncation=True)
+#     for k in dic2.keys():
+#         dic2[k + "2"] = dic2.pop(k)
+#     dic.update(dic2)
+#     return dic
+
 def tokenize_function(examples, tokenizer):
-    return tokenizer(examples["sent1"], padding="max_length", truncation=True), tokenizer(examples["sent2"], padding="max_length", truncation=True)
+    return tokenizer.encode(examples["text1"], examples["text2"], padding="max_length", truncation=True)
+
+
+# def tokenize_function4(examples, tokenizer):
+#     return tokenizer(examples["text1"], padding="max_length", truncation=True)
+#
+# def tokenize_function2(examples, tokenizer):
+#     examples["text1"] = tokenizer(examples["text1"], padding="max_length", truncation=True)
+#     examples["text2"] = tokenizer(examples["text2"], padding="max_length", truncation=True)
+#
+# def tokenize_function3(examples, tokenizer):
+#     return {"text1": tokenizer(examples["text1"], padding="max_length", truncation=True),
+#             "text2": tokenizer(examples["text2"], padding="max_length", truncation=True)}
 
 def binarize_label(examples):
     # can reuse label if using remove_columns in map
@@ -83,42 +106,37 @@ def parse_args():
 
     return parser.parse_args()
 
+# lambda example: {"text1": example["questions.text"][0], "text2": example["questions.text"][1], "label": int(example["is_duplicate"])}
+def func(example):
+    return {"text1": example["questions.text"][0], "text2": example["questions.text"][1], "label": int(example["is_duplicate"])}
+
 def main(args):
     print("starting load", flush=True)
     orig_datasets = load_dataset("quora", data_dir=Path(DATA_FILE), cache_dir=Path(DATA_FILE))
+    orig_datasets['train'] = orig_datasets["train"].shuffle(seed=42, load_from_cache_file=False).select(range(10000))
     orig_datasets = orig_datasets.flatten()
-    orig_datasets["train"] = orig_datasets["train"].add_column("text1", [a[0] for a in orig_datasets["train"]["questions.text"]])
-    orig_datasets["train"] = orig_datasets["train"].add_column("text2", [a[1] for a in orig_datasets["train"]["questions.text"]])
-    orig_datasets["train"] = orig_datasets["train"].add_column("label", [int(a) for a in orig_datasets["train"]["is_duplicate"]])
-    orig_datasets["train"] = orig_datasets["train"].remove_columns(["questions.text", "is_duplicate"])
+    orig_datasets["train"] = orig_datasets["train"].map(func, batched=False)
+    # orig_datasets["train"] = orig_datasets["train"].add_column("text1", [a[0] for a in orig_datasets["train"]["questions.text"]])
+    # orig_datasets["train"] = orig_datasets["train"].add_column("text2", [a[1] for a in orig_datasets["train"]["questions.text"]])
+    # orig_datasets["train"] = orig_datasets["train"].add_column("label", [int(a) for a in orig_datasets["train"]["is_duplicate"]])
+    orig_datasets["train"] = orig_datasets["train"].remove_columns(["questions.id", "questions.text", "is_duplicate"])
     orig_datasets = orig_datasets["train"].train_test_split(test_size=0.2)
     print("finished load", flush=True)
 
     for iter in range(args.i):
         max_score = 0
         raw_datasets = copy.deepcopy(orig_datasets)
-        raw_datasets.shuffle(seed=random.randint(0, 1024), load_from_cache_file=False)
         raw_datasets['train'] = raw_datasets["train"].shuffle(seed=random.randint(0, 1024), load_from_cache_file=False).select(range(args.n))
-        
+        raw_datasets['test'] = raw_datasets["test"].shuffle(seed=random.randint(0, 1024), load_from_cache_file=False).select(range(100)) #TODO: remove/fix
+
         filter_out_example = lambda example: example['label'] not in [0, 1]
 
         raw_datasets['train'] = augment_data('quora', ['text1', 'text2'], raw_datasets['train'], args.multiplier, args.augment_type, DATASET_FILE, PROMPT_FILE, do_filter_score=args.s, do_filter_length=False, filter_out_example = filter_out_example)
         print("finished augment", flush=True)
-        #raw_datasets["train"] = load_dataset("csv", data_files="temp_dataset.csv")["train"].remove_columns(["Unnamed: 0"])
 
-        #raw_datasets["train"]["label"] # list(map(lambda x: int(x>0.5), raw_datasets["train"]["label"]))
-        #raw_datasets["test"]["label"] # list(map(lambda x: int(x>0.5), raw_datasets["test"]["label"]))
-
-
-        #https://huggingface.co/sentence-transformers/bert-base-nli-stsb-mean-tokens
-        # https://huggingface.co/transformers/model_doc/bert.html#bertfornextsentenceprediction
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-        tokenized_datasets = raw_datasets.map(tokenize_function, batched=True, fn_kwargs={'tokenizer':tokenizer})
-        # pickle.dump(tokenized_datasets, open('td.pkl','wb'))
-        # print('exiting...?')
-        # exit(1)
-        #tokenized_datasets = tokenized_datasets.map(binarize_label, batched=True, remove_columns=["label"])
+        tokenized_datasets = raw_datasets.map(tokenize_function, batched=False, fn_kwargs={'tokenizer':tokenizer})
 
         new_features = tokenized_datasets["train"].features.copy()
         new_features["label"] = Value('int32')
@@ -129,7 +147,6 @@ def main(args):
         small_eval_dataset = tokenized_datasets["test"].shuffle(seed=random.randint(0, 1024), load_from_cache_file=False).select(range(100))
         training_args = TrainingArguments("prompts_model", evaluation_strategy="epoch", save_strategy="epoch", num_train_epochs=args.epochs, save_total_limit=2, load_best_model_at_end=True, metric_for_best_model="eval_accuracy")
         metric = load_metric("accuracy")
-        # model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
         model = AutoModelForNextSentencePrediction.from_pretrained("bert-base-uncased")
 
         trainer = Trainer(
