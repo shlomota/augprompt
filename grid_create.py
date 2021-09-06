e06357b37fbf1bda99a29f7302620f7e5e58dba7
shlomota <shlomotannor@mail.tau.ac.il>
2021-09-06 22:02:26 +0300

    added grid_create.py

--- grid_create.py
+++ grid_create.py
@@ -5,6 +5,8 @@ a = "python train_quora.py -m %.1f -n %d -i 5"
 ns = [10, 25, 50, 100]
 ms = [0, 0.25, 0.5, 1, 2]
 
+full_path = False
+
 template = """#! /bin/sh
 #SBATCH --output=/home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/quora_out/%s/out.out
 #SBATCH --error=/home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/quora_out/%s/err.err
@@ -15,6 +17,18 @@ template = """#! /bin/sh
 #SBATCH --gpus=1
 /home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_quora.py -i 5 -n %d -m %.1f
 """
+
+if not full_path:
+    template = """#! /bin/sh
+    #SBATCH --output=%s.out
+    #SBATCH --error=%s.err
+    #SBATCH --partition=studentkillable
+    #SBATCH --job-name=%s
+    #SBATCH --nodes=1
+    #SBATCH --ntasks=1
+    #SBATCH --gpus=1
+    /home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_quora.py -i 5 -n %d -m %.1f
+    """
 
 for n in ns:
     for m in ms:
