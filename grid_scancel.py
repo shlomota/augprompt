880014253fd2a87d229fbfc852cf2c09b768c4c2
shlomota <shlomotannor@mail.tau.ac.il>
2021-09-09 19:32:18 +0300

    added scancel script

---
+++ grid_scancel.py
@@ -0,0 +1,18 @@
+# !python train_quora.py -m 0.5 -n 10 -i 5
+import os
+a = "!python train_quora.py -m %.1f -n %d -i 5"
+a = "python train_quora.py -m %.1f -n %d -i 5"
+ns = [10, 25, 50, 100]
+ms = [0, 0.25, 0.5, 1, 2]
+
+
+
+
+for n in ns:
+    for m in ms:
+        print(a % (m, n))
+        s = "%d_%s" % (n,m)
+        os.system("scancel -n %s" % (s))
+
+
+
