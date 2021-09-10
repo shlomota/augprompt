7492bbee92d74181d0a69d20cd14e08b9328771d
shlomota <shlomotannor@mail.tau.ac.il>
2021-09-10 09:21:01 +0300

    added sleep

--- grid_run_para.py
+++ grid_run_para.py
@@ -1,5 +1,7 @@
 # !python train_paranmt.py -m 0.5 -n 10 -i 5
 import os
+import time
+time.sleep(60 * 60 * 3)
 a = "!python train_paranmt.py -m %s -n %d -i 5"
 a = "python train_paranmt.py -m %s -n %d -i 5"
 ns = [10, 25, 50, 100]
