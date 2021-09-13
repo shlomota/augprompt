e11bf3d8c35c3c803e3d575c14650f62d40e82a0
shlomota <shlomotannor@mail.tau.ac.il>
2021-09-13 06:56:18 +0300

    i_5

--- grid_run_para.py
+++ grid_run_para.py
@@ -1,12 +1,12 @@
-# !python train_paranmt.py -m 0.5 -n 10 -i 1
+# !python train_paranmt.py -m 0.5 -n 10 -i 5
 import os
 import time
 # time.sleep(60 * 60 * 1)
-a = "!python train_paranmt.py -m %s -n %d -i 1"
-a = "python train_paranmt.py -m %s -n %d -i 1"
-a = "python train_paranmt.py -m %s -n %d -i 1"
+a = "!python train_paranmt.py -m %s -n %d -i 5"
+a = "python train_paranmt.py -m %s -n %d -i 5"
+a = "python train_paranmt.py -m %s -n %d -i 5"
 ns = [10, 20, 50, 100]
-ns = [50]
+# ns = [50]
 # ns = [10, 100]
 ms = [0, 0.25, 0.5, 1, 2]
 
@@ -21,7 +21,7 @@ template = """#! /bin/sh
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-/home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_paranmt.py -i 1 -n %d -m %s
+/home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_paranmt.py -i 5 -n %d -m %s
 """
 
 if not full_path:
@@ -33,7 +33,7 @@ if not full_path:
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-/home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_paranmt.py -i 1 -n %d -m %s
+/home/yandex/AMNLP2021/shlomotannor/anaconda3/envs/nli/bin/python /home/yandex/AMNLP2021/shlomotannor/amnlp/shlomo/augprompt/train_paranmt.py -i 5 -n %d -m %s
 """
 
 
